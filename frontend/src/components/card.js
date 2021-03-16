261d54a008e4b2287ff719b9219b518869252e26
Juan Rendon <juan.rendon@zemoga.com>
2021-03-16 14:04:17 -0500

    Chores

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -3,36 +3,37 @@ import { Link } from "gatsby";
 import Img from "gatsby-image";
 
 const Card = ({ article }) => {
+  const {node} = article; 
   return (
-    <Link to={`/article/${article.node.slug}`} className="uk-link-reset">
+    <Link to={`/article/${node.slug}`} className="uk-link-reset">
       <div className="uk-card uk-card-muted">
         <div className="uk-card-media-top">
           <Img
-            fluid={article.node.image.childImageSharp.fluid}
+            fluid={node.image.childImageSharp.fluid}
             imgStyle={{ position: "static" }}
           />
         </div>
         <div className="uk-card-body">
           <p id="category" className="uk-text-uppercase">
-            {article.node.category.name}
+            {node.category.name}
           </p>
           <p id="title" className="uk-text-large">
-            {article.node.title}
+            {node.title}
           </p>
           <div>
             <hr className="uk-divider-small" />
             <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
               <div>
-                {article.node.author.picture && (
+                {node.author.picture && (
                   <Img
-                    fluid={article.node.author.picture.childImageSharp.fluid}
+                    fluid={node.author.picture.childImageSharp.fluid}
                     imgStyle={{ position: "static", borderRadius: "50%" }}
                   />
                 )}
               </div>
               <div className="uk-width-expand">
                 <p className="uk-margin-remove-bottom">
-                  {article.node.author.name}
+                  {node.author.name}
                 </p>
               </div>
             </div>
